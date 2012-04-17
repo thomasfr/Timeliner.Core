348d7219552d1bd54c3a1e5130ecaa47933e6251
Thomas Fritz <fritztho@gmail.com>
2012-04-17 17:36:26 +0200

    WIP

--- test-search.js
+++ test-search.js
@@ -1,10 +1,11 @@
 var util = require('util');
 var search = require('./lib/search');
-var query = "detail";
+
+var query = "comparing";
 
 search(query, function(error, replies) {
    if(error) return console.error(error);
-    else console.log(util.inspect(replies, false, 4, true));
+    else console.log("TESTSEARCH", util.inspect(replies, false, 4, true));
 });
 
 
