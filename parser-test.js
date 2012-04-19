2ddbc908710080374615156854cb767cdd1d3997
Thomas Fritz <fritztho@gmail.com>
2012-04-19 15:12:18 +0200

    FedEx Day Begin

--- parser-test.js
+++ parser-test.js
@@ -1,11 +1,2 @@
-/**
- * Created by JetBrains WebStorm.
- * User: thomas
- * Date: 05.04.12
- * Time: 09:13
- * To change this template use File | Settings | File Templates.
- */
 var parser = require('./lib/parser');
-
-
 console.log(parser.parse("(foo bar) OR (baz bat)"));
