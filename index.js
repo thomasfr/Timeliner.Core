fc944ec7f9da4bffb17e730cdb1c5633999a919f
Thomas Fritz <fritztho@gmail.com>
2012-05-31 19:05:30 +0200

    Refactorings to standalone module Step1. WIP

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-var redis = require('redis');
-var pubSub = redis.createClient();
-var index = require('./lib/index');
+var Indexer = require('./lib/Indexer');
+var search = require('./lib/search');
 
-function onIndexComplete(err, tokens) {
-    "use strict";
-    if (err) {
-        return console.warn(err);
-    } else {
-        return console.log("index complete.\n");
+module.exports = {
+    createIndexer: function (config) {
+        "use strict";
+        return new Indexer(config);
     }
-}
-
-pubSub.subscribe('document:new');
-pubSub.on('message', function (channel, message) {
-    "use strict";
-    try {
-        var document = JSON.parse(message);
-        if (document.id && document.text && document.user) {
-            console.log("new document: ", JSON.stringify(document.text));
-            index(document, function (err, tokens) {
-                if (err) {
-                    return console.warn(err);
-                }
-            });
-        }
-    } catch (error) {
-        console.error(error);
-    }
-});
+};
